2580e519ad8bdf43aa4078aba4359e96e0faec8d
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 10:47:55 +0800

    Update bank_account_secure.py

--- WEEK 6/bank_account_secure.py
+++ WEEK 6/bank_account_secure.py
@@ -1,20 +1,20 @@
-class BankAccount_nbs:
-    def __init__(self_nbs, balance_nbs):
-        self_nbs.__balance = balance_nbs
+class BankAccount_jpa:
+    def __init__(self_jpa, balance_jpa):
+        self_jpa.__balance = balance_jpa
 
-    def deposit_nbs(self_nbs, amount_nbs):
-        self_nbs.__balance += amount_nbs
+    def deposit_jpa(self_jpa, amount_jpa):
+        self_jpa.__balance += amount_jpa
 
-    def withdraw_nbs(self_nbs, amount_nbs):
-        if amount_nbs <= self_nbs.__balance:
-            self_nbs.__balance -= amount_nbs
+    def withdraw_jpa(self_jpa, amount_jpa):
+        if amount_jpa <= self_jpa.__balance:
+            self_jpa.__balance -= amount_jpa
         else:
             print("Insufficient funds")
 
-    def get_balance_nbs(self_nbs):
-        return self_nbs.__balance
+    def get_balance_jpa(self_jpa):
+        return self_jpa.__balance
 
-account_nbs = BankAccount_nbs(5000)
-account_nbs.deposit_nbs(1000)
+account_jpa = BankAccount_jpa(5000)
+account_jpa.deposit_jpa(1000)
 account_nbs.withdraw_nbs(2000)
-print("Balance_nbs:", account_nbs.get_balance_nbs())
+print("Balance_jpa:", account_jpa.get_balance_jpa())
