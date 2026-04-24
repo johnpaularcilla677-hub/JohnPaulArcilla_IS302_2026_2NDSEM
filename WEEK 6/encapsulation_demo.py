e15c2cb6cc8ab99bc3c9a677d0f7bfc1f24d3c26
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 10:45:49 +0800

    Update encapsulation_demo.py

--- WEEK 6/encapsulation_demo.py
+++ WEEK 6/encapsulation_demo.py
@@ -1,14 +1,14 @@
 class Person_jpa:
-    def __init__(self_nbs, name_nbs, age_nbs):
-        self_nbs.__name = name_nbs
-        self_nbs.__age = age_nbs
+    def __init__(self_nbs, name_jpa, age_jpa):
+        self_jpa.__name = name_jpa
+        self_jpa.__age = age_jpa
 
-    def get_name_jpa(self_nbs):
-        return self_nbs.__name
+    def get_name_jpa(self_jpa):
+        return self_jpa.__name
 
-    def get_age_jpa(self_nbs):
-        return self_nbs.__age
+    def get_age_jpa(self_jpa):
+        return self_jpa.__age
 
-p1_nbs = Person_nbs("Maria", 20)
-print("Name:", p1_nbs.get_name_nbs())
-print("Age:", p1_nbs.get_age_nbs())
+p1_nbs = Person_jpa("Maria", 20)
+print("Name:", p1_jpa.get_name_nbs())
+print("Age:", p1_jpa.get_age_nbs())
