4c9f7fdb5854cba3e419997685d341c7535e0676
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 11:04:09 +0800

    Update inheritance_demo.py

--- WEEK 7/inheritance_demo.py
+++ WEEK 7/inheritance_demo.py
@@ -1,14 +1,14 @@
-class Animal_nbs:
-    def __init__(self_nbs, name_nbs):
-        self_nbs.name_nbs = name_nbs
+class Animal_jpa:
+    def __init__(self_jpa, name_jpa):
+        self_jpa.name_jpa = name_jpa
 
-    def speak(self_nbs):
-        print(self_nbs.name_nbs, "makes a sound")
+    def speak(self_jpa):
+        print(self_jpa.name_jpa, "makes a sound")
 
-class Dog_nbs(Animal_nbs):
-    def bark(self_nbs):
-        print(self_nbs.name_nbs, "barks")
+class Dog_jpa(Animal_jpa):
+    def bark(self_jpa):
+        print(self_jpa.name_jpa, "barks")
 
-dog1_nbs = Dog_nbs("Buddy")
-dog1_nbs.speak()
-dog1_nbs.bark()
+dog1_jpa = Dog_jpa("Buddy")
+dog1_jpa.speak()
+dog1_jpa.bark()
