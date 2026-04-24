c1b0e1d322a44bde88e24582650ba03bf6ef6765
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 11:10:33 +0800

    Update student_inheritance.py

--- WEEK 7/student_inheritance.py
+++ WEEK 7/student_inheritance.py
@@ -1,17 +1,17 @@
-class Person_nbs:
-    def __init__(self_nbs, name_nbs, age_nbs):
-        self_nbs.name_nbs = name_nbs
-        self_nbs.age_nbs = age_nbs
+class Person_jpa:
+    def __init__(self_jpa, name_jpa, age_jpa):
+        self_jpa.name_jpa = name_jpa
+        self_jpa.age_jpa = age_jpa
 
-class Student_nbs(Person_nbs):
-    def __init__(self_nbs, name_nbs, age_nbs, course_nbs):
-        super().__init__(name_nbs, age_nbs)
-        self_nbs.course_nbs = course_nbs
+class Student_jpa(Person_jpa):
+    def __init__(self_jpa, name_jpa, age_jpa, course_jpa):
+        super().__init__(name_jpa, age_jpa)
+        self_nbs.course_jpa = course_jpa
 
-    def display_student_nbs(self_nbs):
-        print("Name:", self_nbs.name_nbs)
-        print("Age:", self_nbs.age_nbs)
-        print("Course:", self_nbs.course_nbs)
+    def display_student_jpa(self_jpa):
+        print("Name:", self_jpa.name_jpa)
+        print("Age:", self_jpa.age_jpa)
+        print("Course:", self_jpa.course_jpa)
 
-student1_nbs = Student_nbs("Maria", 20, "BSIS")
-student1_nbs.display_student_nbs()
+student1_jpa = Student_jpa("Maria", 20, "BSIS")
+student1_jpa.display_student_nbs()
