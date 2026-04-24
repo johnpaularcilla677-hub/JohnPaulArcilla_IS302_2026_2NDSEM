91808939213d8faf987da2b7fa9bdf4105b93e62
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 11:15:24 +0800

    Update vehicle_system.py

--- WEEK 7/vehicle_system.py
+++ WEEK 7/vehicle_system.py
@@ -3,13 +3,13 @@ class Vehicle_jpa:
         self_jpa.brand_jpa = brand_jpa
         self_jpa.model_jpa = model_jpa
 
-class Car_nbs(Vehicle_nbs):
-    def __init__(self_nbs, brand_nbs, model_nbs, year_nbs):
-        super().__init__(brand_nbs, model_nbs)
-        self_nbs.year_nbs = year_nbs
+class Car_jpa(Vehicle_jpa):
+    def __init__(self_jpa, brand_jpa, model_jpa, year_jpa):
+        super().__init__(brand_jpa, model_jpa)
+        self_nbs.year_jpa = year_jpa
 
-    def display_car_nbs(self_nbs):
-        print(self_nbs.brand_nbs, self_nbs.model_nbs, self_nbs.year_nbs)
+    def display_car_jpa(self_jpa):
+        print(self_jpa.brand_jpa, self_jpa.model_jpa, self_jpa.year_jpa)
 
-car1_nbs = Car_nbs("Toyota", "Corolla", 2022)
-car1_nbs.display_car_nbs()
+car1_jpa = Car_jpa("Honda", "Civic", 2022)
+car1_jpa.display_car_jpa()
