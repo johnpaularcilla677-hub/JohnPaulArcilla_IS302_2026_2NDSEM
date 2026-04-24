09ddd4cd8f75b698afd6e0824af5922f6b0bbc43
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 11:02:24 +0800

    Update employee_inheritance.py

--- WEEK 7/employee_inheritance.py
+++ WEEK 7/employee_inheritance.py
@@ -1,17 +1,17 @@
-class Employee_nbs:
-    def __init__(self_nbs, name_nbs, salary_nbs):
-        self_nbs.name_nbs = name_nbs
-        self_nbs.salary_nbs = salary_nbs
+class Employee_jpa:
+    def __init__(self_jpa, name_jpa, salary_jpa):
+        self_jpa.name_jpa = name_jpa
+        self_jpa.salary_jpa = salary_jpa
 
-class Manager_nbs(Employee_nbs):
-    def __init__(self_nbs, name_nbs, salary_nbs, department_nbs):
-        super().__init__(name_nbs, salary_nbs)
-        self_nbs.department_nbs = department_nbs
+class Manager_jpa(Employee_jpa):
+    def __init__(self_jpa, name_jpa, salary_jpa, department_jpa):
+        super().__init__(name_jpa, salary_jpa)
+        self_jpa.department_jpa = department_jpa
 
-    def display_manager_nbs(self_nbs):
-        print("Name:", self_nbs.name_nbs)
-        print("Salary:", self_nbs.salary_nbs)
-        print("Department:", self_nbs.department_nbs)
+    def display_manager_jpa(self_jpa):
+        print("Name:", self_jpa.name_jpa)
+        print("Salary:", self_jpa.salary_jpa)
+        print("Department:", self_jpa.department_jpa)
 
-manager1_nbs = Manager_nbs("John", 50000, "IT")
-manager1_nbs.display_manager_nbs()
+manager1_jpa = Manager_jpa("John", 50000, "IT")
+manager1_jpa.display_manager_jpa()
