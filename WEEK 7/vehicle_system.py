e05290bb8c3aeb32bc883ca8e1c1ac4b3babf46c
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 11:12:17 +0800

    Update vehicle_system.py

--- WEEK 7/vehicle_system.py
+++ WEEK 7/vehicle_system.py
@@ -1,7 +1,7 @@
-class Vehicle_nbs:
-    def __init__(self_nbs, brand_nbs, model_nbs):
-        self_nbs.brand_nbs = brand_nbs
-        self_nbs.model_nbs = model_nbs
+class Vehicle_jpa:
+    def __init__(self_jpa, brand_jpa, model_jpa):
+        self_jpa.brand_jpa = brand_jpa
+        self_jpa.model_jpa = model_jpa
 
 class Car_nbs(Vehicle_nbs):
     def __init__(self_nbs, brand_nbs, model_nbs, year_nbs):
