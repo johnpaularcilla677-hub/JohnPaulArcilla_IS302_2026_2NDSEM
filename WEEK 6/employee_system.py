08aed368d781727c3e4b168f1ad03272a220be7e
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2026-04-24 10:49:30 +0800

    Update employee_system.py

--- WEEK 6/employee_system.py
+++ WEEK 6/employee_system.py
@@ -1,15 +1,15 @@
-class Employee_nbs:
-    def __init__(self_nbs, name_nbs):
-        self_nbs.__name = name_nbs
-        self_nbs.__salary = 0
+class Employee_jpa:
+    def __init__(self_jpa, name_jpa):
+        self_jpa.__name = name_jpa
+        self_jpa.__salary = 0
 
-    def set_salary_nbs(self_nbs, salary_nbs):
-        if salary_nbs > 0:
-            self_nbs.__salary = salary_nbs
+    def set_salary_jpa(self_jpa, salary_jpa):
+        if salary_jpa > 0:
+            self_jpa.__salary = salary_jpa
 
-    def get_salary_nbs(self_nbs):
-        return self_nbs.__salary
+    def get_salary_jpa(self_jpa):
+        return self_jpa.__salary
 
-emp_nbs = Employee_nbs("Ana")
-emp_nbs.set_salary_nbs(30000)
-print("Salary:", emp_nbs.get_salary_nbs())
+emp_jpa = Employee_jpa("Ana")
+emp_jpa.set_salary_jpa(30000)
+print("Salary:", emp_jpa.get_salary_jpa())
